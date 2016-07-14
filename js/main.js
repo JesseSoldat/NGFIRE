898781718f4daddce2eabf242bf2492186d052de
jesse soldat <jsoldat@hotmail.com>
2016-07-14 18:41:01 +0700

    npm install modules

---
+++ js/main.js
@@ -0,0 +1,19 @@
+import angular from 'angular';
+import 'angular-ui-router';
+import firebase from 'firebase';
+import angularfire from 'angularfire';
+import $ from 'jquery';
+
+
+console.log('fire');
+
+
+var config = {
+apiKey: "AIzaSyBcDhfji5499fBObaKQtVj2fygUdaE0xiI",
+authDomain: "angularfire-ab896.firebaseapp.com",
+databaseURL: "https://angularfire-ab896.firebaseio.com",
+storageBucket: "",
+};
+firebase.initializeApp(config);
+
+
