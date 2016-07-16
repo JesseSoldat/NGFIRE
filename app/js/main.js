86b48baef8395825d28e2e4cc7321b88c824db1d
jesse soldat <jsoldat@hotmail.com>
2016-07-16 07:36:37 +0700

    used some setTimeout to replica a slow response from the server

--- app/js/main.js
+++ app/js/main.js
@@ -127,46 +127,64 @@ var EditCharCtrl = function EditCharCtrl($firebaseArray, $scope, $stateParams, $
 	var id = $stateParams.id;
 
 	var ref = firebase.database().ref();
+	var data = undefined;
 
-	var data = $firebaseArray(ref);
+	setTimeout(function () {
+		data = $firebaseArray(ref);
+	}, 500);
 
 	//Show only one
 	$scope.temp = $firebaseArray(ref);
 	$scope.tempId = $stateParams.id;
 
-	checkData();
+	checkLength();
+
+	function checkLength() {
+		if (data === undefined) {
+			setTimeout(function () {
+				checkLength();
+			}, 200);
+		} else {
+			(0, _jquery2['default'])('#spin').css('display', 'none');
+			return;
+		}
+	}
+
+	// checkData();
 
 	//Wait for data from firebase
-	function checkData() {
-		if (data.length <= 0) {
-			setTimeout(function () {
-				console.log('waiting...');
-				checkData();
-			}, 500);
-		}
-		getOne();
-	}
+	// function checkData(){
+	// 	if(data.length <= 0){
+	// 		setTimeout(function(){
+	// 			console.log('waiting...');
+	// 			checkData()
+	// 		},500);
+	// 	}
+	// 	getOne();
 
+	// }
+	// getOne();
 	//Get the specific character
-	function getOne() {
-		for (var i = 0; i < data.length; i++) {
-			if (data[i].$id === id) {
-				var singleChar = data[i];
-				$scope.char = singleChar;
+	// function getOne(){
+	// for(let i = 0; i < data.length ; i++){
+	// 	if(data[i].$id === id) {
+	// 		let singleChar = data[i];
+	// 		$scope.char = singleChar;
 
-				$scope.$apply(function () {
-					// $('#editNameBefore').text(singleChar.name);
-					//  $("#inputName").append('<input type="text" placeholder="Name" ng-model="char.name"/>');
-					// $('#editUrlBefore').text(singleChar.url);
-					// $("#inputUrl").append('<input type="text" placeholder="Image URL" ng-model="char.url"/>');
-					(0, _jquery2['default'])('#spin').css('display', 'none');
-					// $('#editName').css('display', 'inline-block');
-					// $('#editUrl').css('display', 'inline-block');
-					// $('#editBtn').css('display', 'inline-block');
-				});
-			}
-		}
-	}
+	// $scope.$apply(function(){
+	// $('#editNameBefore').text(singleChar.name);
+	//  $("#inputName").append('<input type="text" placeholder="Name" ng-model="char.name"/>');
+	// $('#editUrlBefore').text(singleChar.url);
+	// $("#inputUrl").append('<input type="text" placeholder="Image URL" ng-model="char.url"/>');
+	// $('#spin').css('display', 'none');
+	// $('#editName').css('display', 'inline-block');
+	// $('#editUrl').css('display', 'inline-block');
+	// $('#editBtn').css('display', 'inline-block');
+	// }); //$scope.$apply
+
+	// }	//if
+	// } //for
+	// } //getOne
 
 	function editChar(char) {
 		var item = data.$getRecord(char.$id);
