11880c76c946ec9e48c827e73a48b78d61f348de
jesse soldat <jsoldat@hotmail.com>
2016-07-18 08:08:48 +0700

    go firbase storage to work with viewing an image

--- app/js/main.js
+++ app/js/main.js
@@ -452,16 +452,50 @@ exports["default"] = SdkCtrl;
 module.exports = exports["default"];
 
 },{}],10:[function(require,module,exports){
-"use strict";
+'use strict';
 
-Object.defineProperty(exports, "__esModule", {
-  value: true
+Object.defineProperty(exports, '__esModule', {
+	value: true
 });
-var StorageCtrl = function StorageCtrl() {};
-StorageCtrl.$inject = [];
+var StorageCtrl = function StorageCtrl($scope) {
 
-exports["default"] = StorageCtrl;
-module.exports = exports["default"];
+	var storage = firebase.storage();
+
+	var storageRef = storage.ref();
+
+	storageRef.child('costarica04.jpg').getDownloadURL().then(function (url) {
+		console.log(url);
+		$scope.$apply(function () {
+			$scope.costarica04 = url.toString();
+		});
+	})['catch'](function (error) {
+		switch (error.code) {
+			case 'storage/object_not_found':
+				console.log("not found");
+				// File doesn't exist
+				break;
+
+			case 'storage/unauthorized':
+				// User doesn't have permission to access the object
+				console.log('unauthorized');
+				break;
+
+			case 'storage/canceled':
+				// User canceled the upload
+				console.log('canceled');
+				break;
+
+			case 'storage/unknown':
+				// Unknown error occurred, inspect the server response
+				console.log('unknown error');
+				break;
+		}
+	});
+};
+StorageCtrl.$inject = ['$scope'];
+
+exports['default'] = StorageCtrl;
+module.exports = exports['default'];
 
 },{}],11:[function(require,module,exports){
 "use strict";
--- js/ctrl/storage.ctrl.js
+++ js/ctrl/storage.ctrl.js
@@ -1,6 +1,41 @@
-let StorageCtrl = function(){
+let StorageCtrl = function($scope){
+	
+	var storage = firebase.storage();
 
+	var storageRef = storage.ref();
+	
+
+	storageRef.child('costarica04.jpg').getDownloadURL().then(function(url) {
+		console.log(url);
+		$scope.$apply(function(){
+			$scope.costarica04 = url.toString();
+
+		});
+		
+	}).catch(function(error) {
+	  switch (error.code) {
+		    case 'storage/object_not_found':
+		    	console.log("not found");
+		      // File doesn't exist
+		      break;
+
+		    case 'storage/unauthorized':
+		      // User doesn't have permission to access the object
+		      console.log('unauthorized');
+		      break;
+
+		    case 'storage/canceled':
+		      // User canceled the upload
+		      console.log('canceled');
+		      break;
+
+		    case 'storage/unknown':
+		      // Unknown error occurred, inspect the server response
+		      console.log('unknown error');
+		      break;
+		  }
+	});
 };
-StorageCtrl.$inject = [];
+StorageCtrl.$inject = ['$scope'];
 
 export default StorageCtrl;
